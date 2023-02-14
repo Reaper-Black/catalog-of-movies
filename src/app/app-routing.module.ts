c69a6125826c011db8be674b3054e7350766721f
andreschacon <johnstartreck@gmail.com>
2023-02-13 20:11:36 -0600

    agregando imagenes y pagina al home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path:'', pathMatch:'full',redirectTo:'/home'},
+  {path:'', pathMatch:'full',redirectTo:'home'},
   {path:'home',component: HomePageComponent},
   {path:'login',component: LoginComponent}
 ];
