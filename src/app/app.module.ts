375c5519f7964e0e6f6b64b2a6011c15df868bf1
Omar Sanchez <initialshoot@outlook.com>
2023-02-21 19:32:06 -0600

    se convirtio la app en una pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment.prod';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +25,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     ReactiveFormsModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
